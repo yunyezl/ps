ef34ca6045eba9d968a44a933f527c96a85292fd
윤예지 <69361613+yunyezl@users.noreply.github.com>
2022-04-11 22:22:34 +0900

    PRG - 신고 결과 받기
    예전에 푼 게 더 잘 푼듯 ㄱ-

--- problem/PRG/신고 결과 받기.swift
+++ problem/PRG/신고 결과 받기.swift
@@ -27,3 +27,50 @@ func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     }
     return answer
 }
+
+// 22.04.11 (월)
+
+import Foundation
+
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+    
+    var reportInfo: [String: Set<String>] = [:]
+    
+    for r in report {
+        let info = r.split(separator: " ")
+        let (reporter, reported) = (String(info[0]), String(info[1]))
+        if let _ = reportInfo[reporter] {
+            reportInfo[reporter]!.update(with: reported)
+        } else {
+            reportInfo[reporter] = [reported]
+        }
+    }
+    
+    var reporterCount: [String: Int] = [:]
+    for r in reportInfo {
+        for reported in r.value {
+            if let _ = reporterCount[String(reported)] {
+                reporterCount[String(reported)]! += 1
+            } else {
+                reporterCount[String(reported)] = 1
+            }
+        }
+    }
+    
+    var answer: [Int] = []
+    for id in id_list {
+        var result = 0
+        if let _ = reportInfo[id] {
+            for reported in reportInfo[id]! {
+                if let count = reporterCount[reported] {
+                    if count >= k {
+                        result += 1
+                    }
+                }   
+            }
+        }
+        answer.append(result)
+    }
+
+    return answer
+}
